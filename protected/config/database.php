64bb9e1209a3f63b490a56f02da74c00970f104d
John Falcon <falcon@walktopus.com>
2018-08-20 17:48:12 -0500

    Initial commit

---
+++ protected/config/database.php
@@ -0,0 +1,14 @@
+<?php
+
+// This is the database connection configuration.
+return array(
+//	'connectionString' => 'sqlite:'.dirname(__FILE__).'/../data/testdrive.db',
+	// uncomment the following lines to use a MySQL database
+	
+	'connectionString' => 'mysql:host=localhost;ovrmind_db=testdrive',
+	'emulatePrepare' => true,
+	'username' => 'root',
+	'password' => '2R9xKc>xZiY)ardE',
+	'charset' => 'utf8',
+	
+);
